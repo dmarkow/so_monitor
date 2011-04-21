f2e6aab37b3192484f0f5c19c2fe035b30f3e25b
Dylan Markow <dm@petefowler.com>
2011-04-21 08:30:27 -0700

    Initial Commit

---
+++ Stack Exchange Watcher/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  Stack Exchange Watcher
+#
+#  Created by Dylan Markow on 4/21/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
---
+++ Stack Exchange Watcher/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stack Exchange Watcher
+//
+//  Created by Dylan Markow on 4/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
