fc0870ae237150e13fc3b956812e43ec53f85ebf
Dylan Markow <dm@petefowler.com>
2011-04-29 11:01:56 -0700

    More changes

--- Stack Exchange Watcher/AppDelegate.rb
+++ Stack Exchange Watcher/AppDelegate.rb
@@ -7,44 +7,50 @@
 #
 
 class AppDelegate
-    attr_accessor :window
-    def applicationDidFinishLaunching(a_notification)
-        initStatusBar setupMenu
-        # Insert code here to initialize your application
-        reg_data = {"ApplicationName" => "Stack Exchange Watcher", "AllNotifications" => ["Test"], "DefaultNotifications" => ["Test"]}
-        
-        @nc = NSDistributedNotificationCenter.defaultCenter
-        @nc.postNotificationName("GrowlApplicationRegistrationNotification", object:nil, userInfo:reg_data, deliverImmediately:true)
-    end
-    def registrationDictionaryForGrowl
-        
-    end
-    def setupMenu
-        menu = NSMenu.new
-        menu.initWithTitle 'FooApp'
-        mi = NSMenuItem.new
-        mi.title = 'Hello from MacRuby!'
-        mi.action = 'sayHello:'
-        mi.target = self
-        menu.addItem mi
-        menu
-    end
-    def sayHello(sender)
-        puts "test"
-        notification = {'NotificationName' => "Test", 'ApplicationName' => "Stack Exchange Watcher", 'NotificationTitle' => "Test", 'NotificationDescription' => "Test"}
-        notification['NotificationPriority'] = 0
-        notification['NotificationSticky'] = false
-        @nc.postNotificationName('GrowlNotification', object:nil, userInfo:notification, deliverImmediately:true)
+  attr_accessor :window
 
-    end
-    
-    def initStatusBar(menu)
-        status_bar = NSStatusBar.systemStatusBar
-        @status_item = status_bar.statusItemWithLength(NSVariableStatusItemLength)
-        @status_item.setTitle "SO"
-        @status_item.setMenu menu
+  def applicationDidFinishLaunching(a_notification)
+    initStatusBar setupMenu
+    @se_watcher = SEWatcher.new
+    GrowlApplicationBridge.setGrowlDelegate(self)
+  end
 
+  def setupMenu
+    menu = NSMenu.new
+    menu.initWithTitle 'FooApp'
+    mi = NSMenuItem.new
+    mi.title = 'Hello from MacRuby!'
+    mi.action = 'sayHello:'
+    mi.target = self
+    menu.addItem mi
+    menu
+  end
 
-    end
+  def sayHello(sender)
+    GrowlApplicationBridge.notifyWithTitle("Test", description:"This is my test.", notificationName:"Test", iconData:nil, priority:0, isSticky:false,clickContext:"foo")
+    @se_watcher.update
+    puts @se_watcher.content
+  end
+
+  def growlNotificationWasClicked(context)
+    puts context
+  end
+
+  def initStatusBar(menu)
+    status_bar = NSStatusBar.systemStatusBar
+    @status_item = status_bar.statusItemWithLength(NSVariableStatusItemLength)
+    @status_item.setTitle "SO"
+    @status_item.setMenu menu
+  end
 end
 
+class SEWatcher
+  attr_accessor :content
+  def initialize
+    @last_update_at = Time.now
+  end
+
+  def update
+    @content = NSMutableString.alloc.initWithContentsOfURL(NSURL.URLWithString('http://stackoverflow.com'), encoding:NSUTF8StringEncoding, error:nil)
+  end
+end
